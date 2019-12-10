30eb12e65378cc2951801a774d338bbaba4e313e
nhenrycolgate <42982815+nhenrycolgate@users.noreply.github.com>
2019-12-10 14:26:37 -0500

    bottom up to center aligned model

--- models/Roadblock.js
+++ models/Roadblock.js
@@ -19,7 +19,7 @@ function Roadblock(engine,transform,render){
     }
 
     this.Init = function() {
-       
+		//TODO: create collisionbox
     }
 	this.Destroy = function(){
 		
@@ -34,39 +34,53 @@ function RoadblockRender(){
 	this.mesh= new THREE.Object3D();
 	this.mesh.name= "Roadblock";
 	
-	var support1geometry= new THREE.CubeGeometry(40,8,20);
-	var support1material=  new THREE.MeshLambertMaterial(( { color:0xaeb5b8 } ));
-	var support1= new THREE.Mesh(support1geometry,support1material );
-	support1.position.set(0,4,0);
-	this.mesh.add(support1);
-
-	var support2geometry= new THREE.CubeGeometry(40,8,20);
-	var support2material=  new THREE.MeshLambertMaterial(( { color:0xaeb5b8} ));
-	var support2= new THREE.Mesh(support1geometry,support1material );
-	support2.position.set(0,4,-100);
-	this.mesh.add(support2);
-	
-	var leg1geometry = new THREE.CylinderGeometry( 5,5 ,40, 10 );
+	/////////////////
+	var legHeight=40;
+	var leg1geometry = new THREE.CylinderGeometry( 5,5 ,legHeight, 10 );
 	var leg1material = new THREE.MeshLambertMaterial( {color: 0xaeb5b8} );
 	var leg1 = new THREE.Mesh( leg1geometry, leg1material );
-	leg1.position.set(0,24,0);
+	leg1.position.set(0,0,50);
 	this.mesh.add(leg1 );
 
-	var leg2geometry = new THREE.CylinderGeometry( 5,5 ,40, 10 );
+	var leg2geometry = new THREE.CylinderGeometry( 5,5 ,legHeight, 10 );
 	var leg2material = new THREE.MeshLambertMaterial( {color: 0xaeb5b8} );
 	var leg2 = new THREE.Mesh( leg2geometry, leg2material);
-	leg2.position.set(0,24,-100);
+	leg2.position.set(0,0,-50);
 	this.mesh.add(leg2);
+	////////////////////
+	var supportHeight=8;
+	var supportPosY= 0-(legHeight/2);
 	
-	var signgeometry1 = new THREE.CubeGeometry(20,20,65);
+	var support1geometry= new THREE.CubeGeometry(40,supportHeight,20);
+	var support1material=  new THREE.MeshLambertMaterial(( { color:0xaeb5b8 } ));
+	var support1= new THREE.Mesh(support1geometry,support1material );
+	support1.position.set(0,supportPosY-(supportHeight/2),50);
+	this.mesh.add(support1);
+
+	var support2geometry= new THREE.CubeGeometry(40,supportHeight,20);
+	var support2material=  new THREE.MeshLambertMaterial(( { color:0xaeb5b8} ));
+	var support2= new THREE.Mesh(support1geometry,support1material );
+	support2.position.set(0,supportPosY-(supportHeight/2),-50);
+	this.mesh.add(support2);
+	////////////////////
+	var signHeight=20;
+	var signZSize=65;
+	var signPosY= 0+(legHeight/2)+(signHeight/2);
+	var zDist= (leg1.position.z-(signZSize/2))-(leg2.position.z+(signZSize/2));
+	var signPosZ= leg1.position.z-(zDist/2);
+	
+	console.log(signPosZ);
+	
+	
+	var signgeometry1 = new THREE.CubeGeometry(20,signHeight,signZSize);
 	var signmaterial1 =  new THREE.MeshLambertMaterial(( { color:0xff0000} ));
 	this.sign1 = new THREE.Mesh(signgeometry1,signmaterial1);
-	this.sign1.position.set(0,54,-82.5);
+	this.sign1.position.set(0,signPosY,signPosZ);
 	this.mesh.add(this.sign1);
 	
-	var signgeometry2 = new THREE.CubeGeometry( 20,20,65);
+	var signgeometry2 = new THREE.CubeGeometry( 20,signHeight,signZSize);
 	var signmaterial2 =  new THREE.MeshLambertMaterial(( { color:0xff0000} ));
 	this.sign2 = new THREE.Mesh(signgeometry2,signmaterial2);
-	this.sign2.position.set(0,54,-17.5);
+	this.sign2.position.set(0,signPosY,-signPosZ);
 	this.mesh.add(this.sign2);
 }
