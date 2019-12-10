749d5a8c99e7a4d3bb936b816978bfab276ab4bf
nhenrycolgate <42982815+nhenrycolgate@users.noreply.github.com>
2019-12-10 12:44:36 -0500

    bottom up to center alligned model

--- models/Goose.js
+++ models/Goose.js
@@ -29,14 +29,14 @@ function Goose(engine,transform,render){
 			this.particles.push(ptcl.particle);
 		}
 		
-		this.collisionBox= new CollisionBox(transform,40,80,50,[0,30,8]);
+		this.collisionBox= new CollisionBox(transform,40,80,50,[0,0,0]);
 		
 		//collisionbox drawn out for debugging
-		var geometry = new THREE.CubeGeometry(40,80,50);
+		var geometry = new THREE.CubeGeometry(55,80,55);
 		var colors= [0xffffff,0x000000];
 		var material = new THREE.MeshLambertMaterial({color: colors[Math.round(Math.random())], wireframe: true});
 		var obj = new THREE.Mesh(geometry,material);
-		obj.position.set(0,30,8);
+		obj.position.set(0,0,0);
 		render.mesh.add(obj);
 		
     }
@@ -54,84 +54,92 @@ function GooseRender(){
 	this.mesh.name= "Goose";
 	
 	this.lowerbody= new THREE.Object3D();
+	this.middlebody= new THREE.Object3D();
 	this.upperbody= new THREE.Object3D();
-		
-	/*var footgeometry1 = new THREE.CylinderGeometry(8, 8, 5, 3, 3)
-	var footmaterial1 = new THREE.MeshLambertMaterial({color: 0xe08c1d});
-	var foot1 = new THREE.Mesh(footgeometry1, footmaterial1);
-	foot1.position.set(-10,2.5,0);
-	this.lowerbody.add(foot1);
 	
-	var footgeometry2 = new THREE.CylinderGeometry(8, 8, 5, 3, 3)
-	var footmaterial2 = new THREE.MeshLambertMaterial({color: 0xe08c1d});
-	var foot2 = new THREE.Mesh(footgeometry2, footmaterial2);
-	foot2.position.set(10,2.5,0);
-	this.lowerbody.add(foot2);
+	////////////////
+	this.bodyRadius= 15;
+	var bodygeometry= new THREE.SphereGeometry(this.bodyRadius);
+	var bodymaterial= new THREE.MeshLambertMaterial( {color: 0xaeb5b8 , wireframe:false} );
+	this.body= new THREE.Mesh( bodygeometry, bodymaterial);
+	this.middlebody.add(this.body);
+	////////////////////
+	var wingHeight=5;
+	var wingPosY= 0+this.bodyRadius-(this.bodyRadius/1.5);
+	var winggeometry1 = new THREE.CylinderGeometry(10, 10, wingHeight, 3, 3)
+	var wingmaterial1 = new THREE.MeshLambertMaterial({color: 0x000000});
+	var wing1 = new THREE.Mesh(winggeometry1, wingmaterial1);
+	wing1.position.set(-12,wingPosY,0);
+	wing1.rotation.x= Math.PI/2;
+	wing1.rotation.y=-Math.PI/2;
+	wing1.rotation.z=Math.PI/2;
+	this.middlebody.add(wing1);
 	
+	var winggeometry2 = new THREE.CylinderGeometry(10, 10, wingHeight, 3, 3)
+	var wingmaterial2 = new THREE.MeshLambertMaterial({color: 0x000000});
+	var wing2 = new THREE.Mesh(winggeometry2, wingmaterial2);
+	wing2.position.set(12,wingPosY,0);
+	wing2.rotation.x= Math.PI/2;
+	wing2.rotation.y= Math.PI/2;
+	wing2.rotation.z=Math.PI/2;
+	this.middlebody.add(wing2);
+	////////////////////////
 	var legLength= 15;
+	var legPosY= 0-(this.bodyRadius);
 	
 	var leg1geometry = new THREE.CylinderGeometry( 2,2 ,legLength, 10 );
 	var leg1material = new THREE.MeshLambertMaterial( {color: 0xe08c1d} );
 	var leg1 = new THREE.Mesh( leg1geometry, leg1material );
-	leg1.position.set(-10,10,0);
+	leg1.position.set(-10,legPosY,0);
 	this.lowerbody.add(leg1);
 	
 	var leg2geometry = new THREE.CylinderGeometry( 2,2,legLength, 10 );
 	var leg2material = new THREE.MeshLambertMaterial( {color: 0xe08c1d} );
 	var leg2 = new THREE.Mesh( leg2geometry, leg2material);
-	leg2.position.set(10,10,0);
-	this.lowerbody.add(leg2);*/
+	leg2.position.set(10,legPosY,0);
+	this.lowerbody.add(leg2);
+	/////////////////////
+	var footHeight=5;
+	var footPosY= legPosY-(legLength/2)-(footHeight/2);
+	var footgeometry1 = new THREE.CylinderGeometry(8, 8, 5, 3, 3)
+	var footmaterial1 = new THREE.MeshLambertMaterial({color: 0xe08c1d});
+	var foot1 = new THREE.Mesh(footgeometry1, footmaterial1);
+	foot1.position.set(-10,footPosY,0);
+	this.lowerbody.add(foot1);
 	
-	this.bodyRadius= 15;
-	var bodygeometry= new THREE.SphereGeometry(this.bodyRadius);
-	var bodymaterial= new THREE.MeshLambertMaterial( {color: 0xaeb5b8 , wireframe:false} );
-	this.body= new THREE.Mesh( bodygeometry, bodymaterial);
-	this.body.position.set(0,25,0); 
-	/*this.lowerbody.add(this.body);
-	
-	var winggeometry1 = new THREE.CylinderGeometry(10, 10, 5, 3, 3)
-	var wingmaterial1 = new THREE.MeshLambertMaterial({color: 0x000000});
-	var wing1 = new THREE.Mesh(winggeometry1, wingmaterial1);
-	wing1.position.set(-12,32,0);
-	wing1.rotation.x= Math.PI/2;
-	wing1.rotation.y=-Math.PI/2;
-	wing1.rotation.z=Math.PI/2;
-	this.lowerbody.add(wing1);
-	
-	var winggeometry2 = new THREE.CylinderGeometry(10, 10, 5, 3, 3)
-	var wingmaterial2 = new THREE.MeshLambertMaterial({color: 0x000000});
-	var wing2 = new THREE.Mesh(winggeometry2, wingmaterial2);
-	wing2.position.set(12,32,0);
-	wing2.rotation.x= Math.PI/2;
-	wing2.rotation.y= Math.PI/2;
-	wing2.rotation.z=Math.PI/2;
-	this.lowerbody.add(wing2);
-	
-	
-	this.upperbody= new THREE.Object3D();
-	
+	var footgeometry2 = new THREE.CylinderGeometry(8, 8, 5, 3, 3)
+	var footmaterial2 = new THREE.MeshLambertMaterial({color: 0xe08c1d});
+	var foot2 = new THREE.Mesh(footgeometry2, footmaterial2);
+	foot2.position.set(10,footPosY,0);
+	this.lowerbody.add(foot2);
+	//////////////////////////
 	var neckLength=20;
+	var neckPosY= 0+this.bodyRadius;
 	var neckgeometry= new THREE.CylinderGeometry(3,3,neckLength,10);
 	var neckmaterial= new THREE.MeshLambertMaterial( {color: 0xaeb5b8} );
 	var neck= new THREE.Mesh( neckgeometry, neckmaterial);
-	neck.position.set(0,42.5,0);
+	neck.position.set(0,neckPosY,0);
 	this.upperbody.add(neck);
-	
-	var headgeometry= new THREE.SphereGeometry(8);
+	//////////////////////////
+	var headRadius=8;
+	var headPosY= neckPosY+(neckLength/2)+headRadius-1;
+	var headgeometry= new THREE.SphereGeometry(headRadius);
 	var headmaterial= new THREE.MeshLambertMaterial( {color: 0xaeb5b8} );
 	var head= new THREE.Mesh(headgeometry,headmaterial);
-	head.position.set(0,56.5,0);
+	head.position.set(0,headPosY,0);
 	this.upperbody.add(head);
-	
-	var beakgeometry = new THREE.CylinderGeometry(1, 3, 10 );
+	///////////////////////////
+	var beakHeight=10;
+	var beakgeometry = new THREE.CylinderGeometry(1, 3, beakHeight);
 	var beakmaterial=  new THREE.MeshLambertMaterial( {color: 0xe08c1d} );
 	var beak= new THREE.Mesh(beakgeometry,beakmaterial);
-	beak.position.set(0,55,10)
+	beak.position.set(0,headPosY,10)
 	beak.rotation.x = Math.PI/2;
 	this.upperbody.add(beak);
-	
-	this.upperbody.rotation.x= Math.PI/9;*/
+	///////////////////////////
+	this.upperbody.rotation.x= Math.PI/9;
 	
 	this.mesh.add(this.lowerbody);
+	this.mesh.add(this.middlebody);
 	this.mesh.add(this.upperbody);
 }
