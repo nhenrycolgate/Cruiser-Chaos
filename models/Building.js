dad7a932e452033ae26b5258d4211e87d241fba7
nhenrycolgate <42982815+nhenrycolgate@users.noreply.github.com>
2019-12-15 19:15:01 -0500

    Add files via upload

--- models/Building.js
+++ models/Building.js
@@ -1,4 +1,4 @@
-function Building(engine, transform, render) {
+function Building(engine, transform = DefaultTransform(), render = new BuildingRender()) {
 
     GameObject.call(this, engine, transform, render, "BUILDING");
 
@@ -11,17 +11,34 @@ function Building(engine, transform, render) {
 }
 
 function BuildingRender() {
-    this.mesh = new THREE.Object3D();
-    this.mesh.name = "BUILDING";
+	Render.call(this);
+	
+	this.Init = function() {
+		this.mesh.name = "BUILDING";
 
-    var bodyWidth = 20;
-    var bodyHeight = 30;
-    var bodyDepth = 10;
-    var bodyColor = COLORS.red;
+		
+		var bodyHeight = 500;
+		var bodyWidth = bodyHeight/1.5;
+		var bodyDepth = bodyWidth ;
+		
+		var properties= Object.keys(COLORS);
+		var bodyColor= eval("COLORS." + properties[Math.floor(RandomNumberBtwn(0,properties.length))]);
+		console.log(bodyColor);
 
-    var bodyGeometry = new THREE.BoxGeometry(bodyWidth, bodyHeight, bodyDepth, 1, 1, 1);
-    var bodyMaterial = new THREE.MeshLambertMaterial({color: bodyColor});
-    var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+		var bodyGeometry = new THREE.BoxGeometry(bodyWidth, bodyHeight, bodyDepth, 1, 1, 1);
+		var bodyMaterial = new THREE.MeshLambertMaterial({color: bodyColor});
+		var body = new THREE.Mesh(bodyGeometry, bodyMaterial);
+		this.mesh.add(body);
 
-    this.mesh.add(body);
+		var roofHeight= bodyHeight/2;
+		var roofGeometry=  new THREE.CylinderGeometry(bodyWidth/10,bodyDepth,bodyDepth,4);
+		var roofMaterial= new THREE.MeshLambertMaterial({color: bodyColor});
+		var roof= new THREE.Mesh(roofGeometry,roofMaterial);
+		roof.position.set(0,bodyHeight/2+roofHeight/2,0);
+		roof.rotateY(DegreesToRadians(45));
+		
+
+		//this.mesh.add(body);
+		this.mesh.add(roof);
+	}
 }
